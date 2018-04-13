63e3c9810f9528202dd13a47868ff366f9de4607
anderscodes <anders.j.persson@hotmail.com>
2018-04-13 08:16:12 +0000

    Exercises

--- directory.rb
+++ directory.rb
@@ -1,17 +1,28 @@
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please enter the names of the students,then their cohort,  age and a hobby"
   puts "To finish, just hit return twice"
   # create an empty array
-  students = []
+  students = [ ]
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
+    puts "Cohort"
+    cohort = gets.chomp
+    cohort = :November if cohort.empty?
+    puts "Age"
+    age = gets.chomp
+    puts "hobbies?"
+    hobby = gets.chomp
     # add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, age: age, hobby: hobby, cohort: cohort} 
+    if students.count <= 1
+      puts "Now we have #{students.count} student"
+    else
     puts "Now we have #{students.count} students"
+  end
     #get another name from the user
-    name = gets.chomp
+        name = gets.chomp
   end
   # return the array of students
   students
@@ -24,8 +35,14 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  count = -1
+  while true
+    count += 1
+      if students[count] == nil
+      break
+      else
+        puts "#{students[count][:name].center(12)} (#{students[count][:cohort]} cohort) #{students[count][:age]} years old and likes #{students[count][:hobby]}"
+      end
   end
 end
 def print_footer(names)
